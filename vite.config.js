682ad1e1993935d44fd1b6787b1a40bd71724479
Gure-san <azrarafani@gmail.com>
2022-11-25 13:26:43 +0700

    config vite assets handling

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
 /// <reference types="vitest"/>
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").pop();
+          if (/css|png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
 
-  plugins: [
-    svgr(),
-    react(),
-  ],
-})
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+  },
+
+  plugins: [svgr(), react()],
+});
