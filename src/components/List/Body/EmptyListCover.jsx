2c49dfe803898fbac0fc8775a3f1cb5008a0d8fe
Gure-san <azrarafani@gmail.com>
2022-11-25 13:07:04 +0700

    fix assets path

--- src/components/List/Body/EmptyListCover.jsx
+++ src/components/List/Body/EmptyListCover.jsx
@@ -1,23 +1,19 @@
-import noListCover_dark from '../../../assets/List-Section/no-task-dark.png';
-import noListCover_light from '../../../assets/List-Section/no-task-light.png';
+import noListCover_dark from "../../../assets/List-Section/noTask-dark.png";
+import noListCover_light from "../../../assets/List-Section/noTask-light.png";
 
 const SIZE_ICON = 80;
 
 export default function EmptyListCover() {
   return (
-    <section
-    className='w-full flex items-center justify-center rounded-md min-h-[300px] bg-secondary-100'>
-      <div 
-      className='flex flex-wrap items-end justify-center w-max'>
-        <img
-        width={SIZE_ICON}
-        height={SIZE_ICON} 
-        src={noListCover_dark} />
-        <p className=''>
-          Welp...<br/>
+    <section className="w-full flex items-center justify-center rounded-md min-h-[300px] bg-secondary-100">
+      <div className="flex flex-wrap items-end justify-center w-max">
+        <img width={SIZE_ICON} height={SIZE_ICON} src={noListCover_dark} />
+        <p className="">
+          Welp...
+          <br />
           Masih belum ada kategori~
         </p>
       </div>
     </section>
-  )
+  );
 }
